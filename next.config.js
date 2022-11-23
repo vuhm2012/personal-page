47ebc5aa3cb78055a2699a9c35dbace3c93e1a0e
minhvuhoang <hn.minhvu@gmail.com>
2022-11-23 22:44:43 +0700

    fix deploy config

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  "reactStrictMode": true,
-  "swcMinify": true,
-  "images": "akamai"
+  reactStrictMode: true,
+  swcMinify: true,
+  images: {
+    loader: 'akamai'
+  }
 }
 
 module.exports = nextConfig
