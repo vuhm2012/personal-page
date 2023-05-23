5de4bc05e18e3efa02c25a44d33e75301a9d1a6d
vuhm2012 <hn.minhvu@gmail.com>
2023-05-23 23:45:47 +0700

    CreateCreate Next app with new version

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-  images: {
-    loader: 'akamai',
-    path: '/public/images'
-  }
+    output: 'export',
 }
 
 module.exports = nextConfig
